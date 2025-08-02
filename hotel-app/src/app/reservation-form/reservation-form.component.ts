35034262d8c13384a7b05dde6fca3d193a349860
madhusudhanan-jayaram <madhusudhanan.jayaram.com>
2025-08-02 06:58:22 -0500

    feat: added form validation

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   providers: [FormBuilder],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
 export class ReservationFormComponent {
   reservationForm: FormGroup = new FormGroup({});
+  constructor(private formBuilder: FormBuilder) {
+   
+  }
+
+  ngOnInit(): void {
+ this.reservationForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      roomNumber: ['', Validators.required]
+    });  }
+  onSubmit(): void {
+    console.log('Form Submitted', this.reservationForm.value);
+    if (this.reservationForm.valid) {
+      console.log('Reservation Details:', this.reservationForm.value);
+      // Here you can handle the form submission, e.g., send data to a server
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
