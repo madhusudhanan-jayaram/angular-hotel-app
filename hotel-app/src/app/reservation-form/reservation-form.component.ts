7268166cf72c4acfbd3cd1982df84ff5c8f39db8
madhusudhanan-jayaram <madhusudhanan.jayaram.com>
2025-08-02 06:39:33 -0500

    feat: added form components

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
+  imports: [ReactiveFormsModule],
+  providers: [FormBuilder],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
 export class ReservationFormComponent {
-
+  reservationForm: FormGroup = new FormGroup({});
 }
